692aefe44f4fceb75a78dde823a30fb1bcbb9267
mohsen <mohsenaliakbari.atom@hotmail.com>
2020-10-26 10:11:00 +0330

    implement loginFragment

--- app/src/main/java/com/imassage/ui/fragment/login/LoginFragment.kt
+++ app/src/main/java/com/imassage/ui/fragment/login/LoginFragment.kt
@@ -7,14 +7,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.transition.platform.MaterialContainerTransform
+import com.haroldadmin.cnradapter.NetworkResponse
 import com.imassage.R
 import com.imassage.databinding.FragmentLoginBinding
 import com.imassage.ui.base.ScopedFragment
+import kotlinx.android.synthetic.main.fragment_login.*
+import kotlinx.coroutines.launch
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.x.closestKodein
+import org.kodein.di.generic.instance
 
-class LoginFragment : ScopedFragment() {
+class LoginFragment : ScopedFragment() , KodeinAware {
+    override val kodein: Kodein by closestKodein()
+    private val viewModelFactory: LoginViewModelFactory by instance()
+
     private lateinit var viewModel: LoginViewModel
     private lateinit var binding : FragmentLoginBinding
     private lateinit var navController: NavController
@@ -29,16 +40,40 @@ class LoginFragment : ScopedFragment() {
         return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         navController = findNavController()
         transitionAnimationEnter()
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this , viewModelFactory).get(LoginViewModel::class.java)
+        bindUI()
+        onClickListeners()
+    }
+
+    private fun bindUI() = launch {
+
+    }
+    private fun onClickListeners(){
+        fra_login_go_to_signUp.setOnClickListener{
+            navController.navigate(R.id.action_loginFragment_to_signUpFragment)
+        }
+        fra_login_LoginButton.setOnClickListener{
+            loginIntoServer()
+        }
+    }
+    private fun loginIntoServer() = launch {
+        val phone = fra_login_mobile.text.toString()
+        val password = fra_login_password.text.toString()
+        when(val calback = viewModel.login(phone , password)){
+            is NetworkResponse.Success -> {
+                navController.navigate(R.id.action_loginFragment_to_phoneVerificationFragment ,
+                        bundleOf("verification_type" to "login")
+                )
+            }
+        }
     }
 
 
--- app/src/main/java/com/imassage/ui/fragment/login/LoginViewModel.kt
+++ app/src/main/java/com/imassage/ui/fragment/login/LoginViewModel.kt
@@ -1,7 +1,11 @@
 package com.imassage.ui.fragment.login
 
 import androidx.lifecycle.ViewModel
+import com.imassage.data.repository.UserInformationRepository
 
-class LoginViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class LoginViewModel(
+        private val userInformationRepository: UserInformationRepository
+) : ViewModel() {
+    suspend fun login(phone: String , password: String) =
+            userInformationRepository.login(phone , password)
 }
