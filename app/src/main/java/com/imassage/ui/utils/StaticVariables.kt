13fbd77c5bda1049eaf00095442cd601f727c1bb
mohsen <mohsenaliakbari.atom@hotmail.com>
2020-11-06 19:07:30 +0330

    update StaticVariables & DataRepository

---
+++ app/src/main/java/com/imassage/ui/utils/StaticVariables.kt
@@ -0,0 +1,7 @@
+package com.imassage.ui.utils
+
+object StaticVariables{
+    const val VERIFICATION_CODE_FRAGMENT  = "VERIFICATION_CODE_FRAGMENT"
+    const val SPLASH_FRAGMENT = "SPLASH_FRAGMENT"
+    const val SOURCE_FRAGMENT = "SOURCE_FRAGMENT"
+}
