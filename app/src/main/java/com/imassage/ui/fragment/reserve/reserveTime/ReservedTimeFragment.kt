0b497d7e510a5554c80584658e6d69a63bcd7af0
mohsen <mohsenaliakbari.atom@hotmail.com>
2020-11-12 19:03:42 +0330

    Impl ReservedTimeFragment

--- app/src/main/java/com/imassage/ui/fragment/reserve/reserveTime/ReservedTimeFragment.kt
+++ app/src/main/java/com/imassage/ui/fragment/reserve/reserveTime/ReservedTimeFragment.kt
@@ -6,23 +6,46 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import com.imassage.R
+import com.imassage.databinding.FragmentReservedTimeBinding
+import com.imassage.ui.base.ScopedFragment
+import kotlinx.coroutines.launch
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.x.closestKodein
+import org.kodein.di.generic.instance
 
-class ReservedTimeFragment : Fragment() {
+class ReservedTimeFragment : ScopedFragment() , KodeinAware{
+    override val kodein: Kodein by closestKodein()
+    private val viewModelFactory: ReservedTimeViewModelFactory by instance()
 
     private lateinit var viewModel: ReservedTimeViewModel
+    private lateinit var binding: FragmentReservedTimeBinding
+    private lateinit var navController: NavController
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_reserved_time, container, false)
+        binding = FragmentReservedTimeBinding.inflate(inflater , container , false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = findNavController()
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(ReservedTimeViewModel::class.java)
-        // TODO: Use the ViewModel
+        viewModel = ViewModelProvider(this , viewModelFactory).get(ReservedTimeViewModel::class.java)
+        bindUI()
+    }
+    private fun bindUI() = launch {
+
     }
 
 }
