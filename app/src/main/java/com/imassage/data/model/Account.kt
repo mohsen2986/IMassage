ef5984a4b981470855cb7b9b5e4c926b9b5748ed
mohsen <mohsenaliakbari.atom@hotmail.com>
2020-11-12 18:58:38 +0330

    Impl models Account & AnswerRequest

---
+++ app/src/main/java/com/imassage/data/model/Account.kt
@@ -0,0 +1,12 @@
+package com.imassage.data.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Account(
+        @SerializedName("phone")
+        val phone: String ,
+        @SerializedName("name")
+        val name: String ,
+        @SerializedName("family")
+        val family: String
+)
