4b13256ac38d4de82a80721b1be2595f2030d6e3
mohsen <mohsenaliakbari.atom@hotmail.com>
2020-11-20 14:26:52 +0330

    update DataRepository.kt

--- app/src/main/java/com/imassage/data/repository/DataRepository.kt
+++ app/src/main/java/com/imassage/data/repository/DataRepository.kt
@@ -1,13 +1,31 @@
 package com.imassage.data.repository
 
+import android.util.Log
+import android.widget.Toast
+import com.haroldadmin.cnradapter.NetworkResponse
+import com.imassage.data.model.MainPage
 import com.imassage.data.remote.api.AuthApiInterface
 import com.imassage.data.remote.model.AnswerRequest
+import com.imassage.data.remote.model.MainPageResponse
 import com.imassage.ui.base.lazyDeferred
 
 class DataRepository(
         private val apiInterface: AuthApiInterface
 ){
     // main page data
+    val mainPage by lazyDeferred {
+        when(val callback = apiInterface.mainPage()){
+            is NetworkResponse.Success -> callback.body.datas
+            else -> MainPage(listOf() , listOf() , listOf())
+        }
+    }
+    // packages data
+    val packages by lazyDeferred {
+        when(val callback = apiInterface.packages()){
+            is NetworkResponse.Success -> callback.body.datas
+            else -> listOf()
+        }
+    }
     suspend fun mainPage() =
             apiInterface.mainPage()
 
@@ -17,10 +35,12 @@ class DataRepository(
     suspend fun packages() =
             apiInterface.packages()
 
+    suspend fun questions() =
+            apiInterface.questions()
+
     suspend fun answer(answers:AnswerRequest) =
             apiInterface.answers(answers)
 
     // update account
-    suspend fun updateAccount(name:String? , family: String?) =
-            apiInterface.updateAccount(name , family)
+
 }
