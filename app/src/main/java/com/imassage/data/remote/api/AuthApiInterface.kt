47cd939258c492d51a551aecceef268e9aaba485
mohsen <mohsenaliakbari.atom@hotmail.com>
2020-11-20 14:28:29 +0330

    update AuthApiInterface.kt

--- app/src/main/java/com/imassage/data/remote/api/AuthApiInterface.kt
+++ app/src/main/java/com/imassage/data/remote/api/AuthApiInterface.kt
@@ -1,8 +1,7 @@
 package com.imassage.data.remote.api
 
 import com.haroldadmin.cnradapter.NetworkResponse
-import com.imassage.data.model.Account
-import com.imassage.data.model.Form
+import com.imassage.data.model.*
 import com.imassage.data.remote.model.*
 import retrofit2.http.*
 
@@ -23,6 +22,11 @@ interface AuthApiInterface{
     suspend fun packages(
     ): NetworkResponse<PackageResponse, ErrorResponse>
 
+    // get questions
+    @GET("questions")
+    suspend fun questions(
+    ): NetworkResponse<QuestionResponse , ErrorResponse>
+
     // send answers
     @POST("questions")
     suspend fun answers(
@@ -36,6 +40,68 @@ interface AuthApiInterface{
     )
     suspend fun updateAccount(
         @Query("name") name: String? ,
-        @Query("family") family: String?
+        @Query("family") family: String? ,
+        @Query("gender") gender: String?
     ):NetworkResponse<Account, ErrorResponse>
+
+    // ger User information
+    @GET("user_info")
+    suspend fun userInformation(
+    ): NetworkResponse<Account , ErrorResponse>
+
+
+    // get available dates for reserve
+    @GET("available_dates")
+    suspend fun availableDates(
+    ): NetworkResponse<DatesResponse , ErrorResponse>
+
+    @POST("available_date_time")
+    suspend fun availableDateTime(
+            @Query("date") date: String
+    ): NetworkResponse<DateTimes, ErrorResponse>
+    // check time
+    @POST("check_reserve_time")
+    suspend fun checkSelectedTime(
+            @Query("reserved_time_date_id") date: String ,
+            @Query("time") time: String ,
+            @Query("massage_id") massageId: String ,
+            @Query("gender") gender: String
+    ): NetworkResponse<SuccessResponse, ErrorResponse>
+    // start pay transactions
+    @POST("transaction")
+    suspend fun transaction(
+            @Query("package_id") packageIs: String ,
+            @Query("offer_code") offerCode: String?
+    ): NetworkResponse<Transactions, ErrorResponse>
+    // pay transaction
+    @POST("payTransaction")
+    suspend fun payTransaction(
+            @Query("transaction_id") transactionId: String ,
+            @Query("ref_id") refId: String
+    ): NetworkResponse<Transactions , ErrorResponse>
+    // orders history
+    @GET("orders_history")
+    suspend fun ordersHistory(
+    ): NetworkResponse<OrdersResponse, ErrorResponse>
+    //  get orders
+    @POST("orders")
+    suspend fun orders(
+    ): NetworkResponse<OrdersResponse , ErrorResponse>
+    // make offer
+    @POST("transaction_offer")
+    suspend fun setOffer(
+            @Query("transaction_id") transactionId: String ,
+            @Query("offer_code") offerCode: String
+    ): NetworkResponse<Transactions , ErrorResponse>
+    // order
+    @POST("order")
+    suspend fun order(
+            @Query("filled_form_id") filledFormId: String ,
+            @Query("time") time: String ,
+            @Query("reserved_time_date_id") reservedTimeDateId: String ,
+            @Query("massage_id") massageId: String ,
+            @Query("package_id") packageId: String,
+            @Query("transactions_id") transactionId: String,
+            @Query("gender") gender: String
+    ): NetworkResponse<Order , ErrorResponse>
 }
