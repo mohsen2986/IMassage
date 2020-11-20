c8a6e7d4b0a4801bd5a0151bd4cbf196f32ee0b6
mohsen <mohsenaliakbari.atom@hotmail.com>
2020-11-20 14:40:47 +0330

    Impl AccountRepository

---
+++ app/src/main/java/com/imassage/data/repository/AccountRepository.kt
@@ -0,0 +1,27 @@
+package com.imassage.data.repository
+
+import com.haroldadmin.cnradapter.NetworkResponse
+import com.imassage.data.model.Account
+import com.imassage.data.model.MainPage
+import com.imassage.data.remote.api.AuthApiInterface
+import com.imassage.ui.base.lazyDeferred
+
+class AccountRepository(
+        private val apiInterface: AuthApiInterface
+){
+    // get Account information
+    // TODO GET ACCOUNT INFORMATION
+    suspend fun userInformation() =
+            apiInterface.userInformation()
+    
+    // update Account information
+    suspend fun updateAccount(name:String? , family: String? , gender: String?) =
+            apiInterface.updateAccount(name , family , gender)
+
+    val account by lazyDeferred {
+        when(val callback = apiInterface.userInformation()){
+            is NetworkResponse.Success -> callback.body
+            else -> Account("unknown" , "unknown" ,"unknown" , "unknown" , "true")
+        }
+    }
+}
