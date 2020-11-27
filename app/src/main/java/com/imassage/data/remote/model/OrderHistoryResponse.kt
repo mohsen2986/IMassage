7a063820a0546439f736525b1c6dfd5f38cc3641
mohsen <mohsenaliakbari.atom@hotmail.com>
2020-11-28 00:09:44 +0330

    add reserve time to OrderHistoryResponse.kt

--- app/src/main/java/com/imassage/data/remote/model/OrderHistoryResponse.kt
+++ app/src/main/java/com/imassage/data/remote/model/OrderHistoryResponse.kt
@@ -1,8 +1,9 @@
 package com.imassage.data.remote.model
 
 import com.google.gson.annotations.SerializedName
+import com.imassage.data.model.Order
 
 data class OrderHistoryResponse(
         @SerializedName("datas")
-        val datas: List<Data>
+        val datas: List<Order>
 )
