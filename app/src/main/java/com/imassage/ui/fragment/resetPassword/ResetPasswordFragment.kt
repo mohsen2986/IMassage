92f43298dd9756355f26276bcea4136103a65968
mohsen <mohsenaliakbari.atom@hotmail.com>
2020-11-28 00:06:07 +0330

    add ResetPasswordFragment.kt

---
+++ app/src/main/java/com/imassage/ui/fragment/resetPassword/ResetPasswordFragment.kt
@@ -0,0 +1,32 @@
+package com.imassage.ui.fragment.resetPassword
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.imassage.R
+
+class ResetPasswordFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = ResetPasswordFragment()
+    }
+
+    private lateinit var viewModel: ResetPasswordViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_reset_password, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(ResetPasswordViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/imassage/ui/fragment/resetPassword/ResetPasswordViewModel.kt
@@ -0,0 +1,7 @@
+package com.imassage.ui.fragment.resetPassword
+
+import androidx.lifecycle.ViewModel
+
+class ResetPasswordViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
