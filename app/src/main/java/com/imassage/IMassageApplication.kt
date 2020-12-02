dc971ecac6f7515b7db3cc608e224a320990f2fc
mohsen <mohsenaliakbari.atom@hotmail.com>
2020-12-02 23:44:06 +0330

    update and rebase

--- app/src/main/java/com/imassage/IMassageApplication.kt
+++ app/src/main/java/com/imassage/IMassageApplication.kt
@@ -1,23 +1,31 @@
 package com.imassage
 
 import android.app.Application
+import android.widget.Toast
 import com.facebook.stetho.Stetho
 import com.imassage.data.database.sharedPreferences.Preferences
 import com.imassage.data.remote.*
 import com.imassage.data.remote.api.ApiInterface
 import com.imassage.data.remote.api.AuthApiInterface
-import com.imassage.data.repository.DataRepository
-import com.imassage.data.repository.TokenRepository
-import com.imassage.data.repository.UserInformationRepository
+import com.imassage.data.repository.*
+import com.imassage.ui.dialog.account.editFamily.EditAccountFamilyDialogViewModelFactory
+import com.imassage.ui.dialog.account.editGender.EditAccountGenderDialogViewModelFactory
+import com.imassage.ui.dialog.account.editImage.EditAccountImageDialogViewModelFactory
+import com.imassage.ui.dialog.account.editName.EditAccountNameDialogViewModelFactory
+import com.imassage.ui.dialog.resetPassword.ResetPasswordViewModelFactory
 import com.imassage.ui.fragment.account.AccountViewModelFactory
 import com.imassage.ui.fragment.login.LoginViewModelFactory
 import com.imassage.ui.fragment.mainPage.MainPageViewModelFactory
+import com.imassage.ui.fragment.paymentGateway.PaymentGatewayViewModelFactory
 import com.imassage.ui.fragment.phoneVerification.PhoneVerificationViewModelFactory
+import com.imassage.ui.fragment.receipt.ReceiptViewModelFactory
 import com.imassage.ui.fragment.registerForm.RegisterFormViewModelFactory
 import com.imassage.ui.fragment.reserve.massage.MassageViewModelFactory
 import com.imassage.ui.fragment.reserve.packages.PackagesViewModelFactory
 import com.imassage.ui.fragment.reserve.question.QuestionViewModelFactory
 import com.imassage.ui.fragment.reserve.reserveTime.ReservedTimeViewModelFactory
+import com.imassage.ui.fragment.reserveView.ReserveViewViewModelFactory
+import com.imassage.ui.fragment.resetPassword.ResetPasswordViewModel
 import com.imassage.ui.fragment.signUp.SignUpViewModelFactory
 import com.imassage.ui.fragment.splashScreen.SplashScreenViewModelFactory
 import okhttp3.OkHttpClient
@@ -45,24 +53,35 @@ class IMassageApplication(
         bind() from singleton { TokenInterceptor(instance() , instance() , instance()) }
         bind() from singleton { TokenAuthenticator(instance() , instance())}
         bind<AuthApiInterface>() with singleton { CreateAuthApiInterface(instance() , instance()).invoke() }
-
         // ViewModel factories
-        bind() from provider { SplashScreenViewModelFactory(instance())}
+        bind() from provider { SplashScreenViewModelFactory(instance() , instance())}
         bind() from provider { SignUpViewModelFactory() }
         bind() from provider { RegisterFormViewModelFactory(instance()) }
         bind() from provider { LoginViewModelFactory(instance()) }
         bind() from provider { PhoneVerificationViewModelFactory(instance())}
-        bind() from provider { MainPageViewModelFactory(instance() , instance()) }
-        bind() from provider { MassageViewModelFactory(instance()) }
-        bind() from provider { PackagesViewModelFactory(instance()) }
+        bind() from provider { MainPageViewModelFactory(instance() , instance() , instance() , instance()) }
+        bind() from provider { MassageViewModelFactory(instance() , instance()) }
+        bind() from provider { PackagesViewModelFactory(instance() , instance()) }
         bind() from provider { QuestionViewModelFactory(instance()) }
-        bind() from provider { ReservedTimeViewModelFactory() }
+        bind() from provider { ReservedTimeViewModelFactory(instance()) }
         bind() from provider { AccountViewModelFactory(instance()) }
+        bind() from provider { ReceiptViewModelFactory(instance() , instance() , instance()) }
+        bind() from provider { PaymentGatewayViewModelFactory(instance()) }
+        bind() from provider { ReserveViewViewModelFactory(instance()) }
+        bind() from provider { com.imassage.ui.fragment.resetPassword.ResetPasswordViewModelFactory(instance())}
+        // DIALOGS
+        bind() from provider { EditAccountNameDialogViewModelFactory(instance())}
+        bind() from provider { EditAccountFamilyDialogViewModelFactory(instance())}
+        bind() from provider { EditAccountImageDialogViewModelFactory(instance())}
+        bind() from provider { EditAccountGenderDialogViewModelFactory(instance())}
+        bind() from provider { ResetPasswordViewModelFactory(instance())}
 
         // repositories
         bind() from singleton { UserInformationRepository(instance() , instance())}
         bind() from singleton { TokenRepository(instance())}
         bind() from singleton { DataRepository(instance())}
+        bind() from singleton { AccountRepository(instance()) }
+        bind() from singleton { OrderRepository(instance())}
 
     }
     override fun onCreate() {
